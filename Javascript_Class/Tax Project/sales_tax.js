dde82260dadbb852d941bf1fefcc9b57b03ff8bb
GalacticElephant <87489059+GalacticElephant@users.noreply.github.com>
2023-09-13 09:37:43 -0500

    Comments

---
+++ Javascript_Class/Tax Project/sales_tax.js
@@ -0,0 +1,30 @@
+const $ = selector => document.querySelector(selector);
+
+const processEntries = () => {
+	// get user entries
+	
+	// if-elseif-else to validate inputs, calculate, and display results
+	    
+    // move focus
+
+};
+
+//  clear functions
+
+
+const clearEntries = () => {
+	// call clear functions
+
+	// clear all text boxes
+   
+	// move focus
+
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+	// add event handlers
+
+	// move focus
+
+});
+
