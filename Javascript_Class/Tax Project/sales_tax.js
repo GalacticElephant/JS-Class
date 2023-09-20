c42a6af60008c1cd657c06e84f92360b7bfc0f00
GalacticElephant <87489059+GalacticElephant@users.noreply.github.com>
2023-09-20 10:00:44 -0500

    It works now
    The comments are just me salvaging a tiny shred of sanity

--- Javascript_Class/Tax Project/sales_tax.js
+++ Javascript_Class/Tax Project/sales_tax.js
@@ -1,30 +1,36 @@
-const $ = selector => document.querySelector(selector);
-
-const processEntries = () => {
-	// get user entries
-	
-	// if-elseif-else to validate inputs, calculate, and display results
-	    
-    // move focus
-
-};
-
-//  clear functions
-
-
-const clearEntries = () => {
-	// call clear functions
-
-	// clear all text boxes
-   
-	// move focus
-
-};
-
-document.addEventListener("DOMContentLoaded", () => {
-	// add event handlers
-
-	// move focus
-
-});
-
+document.addEventListener("DOMContentLoaded", function () {
+	// Put things in things
+	const subtotalInput = document.getElementById("subtotal");
+	const taxRateInput = document.getElementById("tax_rate");
+	const salesTaxInput = document.getElementById("sales_tax");
+	const totalInput = document.getElementById("total");
+  
+	// Find clicky bois
+	const calculateButton = document.getElementById("calculate");
+	const clearButton = document.getElementById("clear");
+  
+	// Let clicky bois do things
+	calculateButton.addEventListener("click", function () {
+	  // Steal user input
+	  const subtotal = parseFloat(subtotalInput.value);
+	  const taxRate = parseFloat(taxRateInput.value);
+  
+	  // Math the things
+	  const salesTax = (subtotal * (taxRate / 100)).toFixed(2);
+	  const total = (subtotal + parseFloat(salesTax)).toFixed(2);
+  
+	  // Display the final thing
+	  salesTaxInput.value = salesTax;
+	  totalInput.value = total;
+	});
+  
+		// Clicky boi 2 electric boogaloo
+	clearButton.addEventListener("click", function () {
+	  // Forget the things
+	  subtotalInput.value = "";
+	  taxRateInput.value = "";
+	  salesTaxInput.value = "";
+	  totalInput.value = "";
+	});
+  });
+  
