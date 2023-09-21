84002f9fa7ab2d5bbac10524a18fd40bf854c5f6
GalacticElephant <87489059+GalacticElephant@users.noreply.github.com>
2023-09-20 20:07:55 -0500

    Update sales_tax.js

--- Javascript_Class/Tax Project/sales_tax.js
+++ Javascript_Class/Tax Project/sales_tax.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
 	  totalInput.value = total;
 	});
   
-	  // Nuke the things
+	// Nuke the things
 	clearButton.addEventListener("click", function () {
 	  subtotalInput.value = "";
 	  taxRateInput.value = "";
