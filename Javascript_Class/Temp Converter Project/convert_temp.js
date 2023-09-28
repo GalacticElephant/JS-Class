c245cf993b58bb6f8cb172be2fdfa5b0caa3f357
GalacticElephant <87489059+GalacticElephant@users.noreply.github.com>
2023-09-28 08:21:44 -0500

    css cursed

--- Javascript_Class/Temp Converter Project/convert_temp.js
+++ Javascript_Class/Temp Converter Project/convert_temp.js
@@ -1,48 +1,64 @@
-"use strict";
-const $ = selector => document.querySelector(selector);
+// Wait for the DOM to be fully loaded before attaching event listeners
+document.addEventListener("DOMContentLoaded", function () {
+	// Get references to the input elements and the convert button
+	const toCelsiusRadio = document.getElementById("to_celsius");
+	const toFahrenheitRadio = document.getElementById("to_fahrenheit");
+	const degreesEnteredInput = document.getElementById("degrees_entered");
+	const degreesComputedInput = document.getElementById("degrees_computed");
+	const degreeLabel1 = document.getElementById("degree_label_1");
+	const degreeLabel2 = document.getElementById("degree_label_2");
+	const convertButton = document.getElementById("convert");
+  
+	// Attach a click event listener to the convert button
+	convertButton.addEventListener("click", function () {
+	  // Get the user's input and convert it based on the selected radio button
+	  const degrees = parseFloat(degreesEnteredInput.value);
+  
+	  if (toCelsiusRadio.checked) {
+		// Convert Fahrenheit to Celsius
+		const celsius = (degrees - 32) * (5 / 9);
+		degreesComputedInput.value = celsius.toFixed(2) + " °C";
+		degreeLabel1.textContent = "Enter F degrees:";
+		degreeLabel2.textContent = "Degrees Celsius:";
+	  } else if (toFahrenheitRadio.checked) {
+		// Convert Celsius to Fahrenheit
+		const fahrenheit = (degrees * 9 / 5) + 32;
+		degreesComputedInput.value = fahrenheit.toFixed(2) + " °F";
+		degreeLabel1.textContent = "Enter C degrees:";
+		degreeLabel2.textContent = "Degrees Fahrenheit:";
+	  }
+	});
+  
+	// Initially set labels based on the default selected radio button
+	if (toCelsiusRadio.checked) {
+	  degreeLabel1.textContent = "Enter F degrees:";
+	  degreeLabel2.textContent = "Degrees Celsius:";
+	} else if (toFahrenheitRadio.checked) {
+	  degreeLabel1.textContent = "Enter C degrees:";
+	  degreeLabel2.textContent = "Degrees Fahrenheit:";
+	}
+  
+	// Attach change event listeners to the radio buttons to update labels
+	toCelsiusRadio.addEventListener("change", function () {
+	  degreeLabel1.textContent = "Enter F degrees:";
+	  degreeLabel2.textContent = "Degrees Celsius:";
+	});
+  
+	toFahrenheitRadio.addEventListener("change", function () {
+	  degreeLabel1.textContent = "Enter C degrees:";
+	  degreeLabel2.textContent = "Degrees Fahrenheit:";
+	});
+  });
 
-/*********************
-*  helper functions  *
-**********************/
-const calculateCelsius = temp => (temp-32) * 5/9;
-const calculateFahrenheit = temp => temp * 9/5 + 32;
 
-const toggleDisplay = (label1Text, label2Text) => {
-	
-	// update labels and clear disabled textbox
-	
-	// select text in degrees textbox
+  // JavaScript
+const main = document.querySelector("main");
 
-}
-
-/****************************
-*  event handler functions  *
-*****************************/
-const convertTemp = () => {   
-
-	// get user entry
-	
-    // Use an 'if/else'
-	
-	// Validate user entery - check for numeric, notify user of error, clear previously computed value, and select text in degrees textbox	
-	
-	// if entry is valid
-	// clear any previous error message
-	// compute and display temp based on which radio button is checked
-	// select text in degrees textbox
-	
-
-};
-
-const toCelsius = () => toggleDisplay("Enter F degrees:", "Degrees Celsius:");
-const toFahrenheit = () => toggleDisplay("Enter C degrees:", "Degrees Fahrenheit:");
-
-document.addEventListener("DOMContentLoaded", () => {
-	// add event handlers
-	$("#convert").addEventListener("click", convertTemp);
-    $("#to_celsius").addEventListener("click", toCelsius);
-    $("#to_fahrenheit").addEventListener("click", toFahrenheit);
-	
-	// move focus
-	$("#degrees_entered").focus();
+document.addEventListener("mousemove", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+  
+  main.style.transform = `translate(${x}px, ${y}px) rotate(180deg)`;
 });
+
+  
