809d32fc5866050bc3e6913500615a517edf0e6a
GalacticElephant <87489059+GalacticElephant@users.noreply.github.com>
2023-10-04 02:42:09 -0500

    perfection

--- Javascript_Class/Clock Project /clock.js
+++ Javascript_Class/Clock Project /clock.js
@@ -1,14 +1,34 @@
 "use strict";
-const $ = selector => document.querySelector(selector);
 
-const padSingleDigit = num => num.toString().padStart(2, "0");
+const $ = (selector) => document.querySelector(selector);
+
+const padSingleDigit = (num) => num.toString().padStart(2, "0");
 
 const displayCurrentTime = () => {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  const ampm = hours >= 12 ? "PM" : "AM";
 
+  const hoursDisplay = $("#hours");
+  const minutesDisplay = $("#minutes");
+  const secondsDisplay = $("#seconds");
+  const ampmDisplay = $("#ampm");
+
+  const formattedHours = padSingleDigit(hours % 12 || 12);
+  const formattedMinutes = padSingleDigit(minutes);
+  const formattedSeconds = padSingleDigit(seconds);
+
+  hoursDisplay.textContent = formattedHours;
+  minutesDisplay.textContent = formattedMinutes;
+  secondsDisplay.textContent = formattedSeconds;
+  ampmDisplay.textContent = ampm;
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-	// set initial clock display and then set interval timer to display
-    // new time every second. Don't store timer object because it 
-    // won't be needed - clock will just run.
+  // Set initial clock display and then set an interval timer to display
+  // the new time every second.
+  displayCurrentTime();
+  setInterval(displayCurrentTime, 1000);
 });
